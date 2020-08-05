cec2806bd1b3db2dd0b57b710020fab82570757f
littlerzrz <renxinyi.design@gmail.com>
2020-08-06 00:03:48 +0800

    W9D1 canvas practice modifications

--- W9D1/canvas_practice/practice.js
+++ W9D1/canvas_practice/practice.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", function(){
-
+    const canvas = document.getElementById('mycanvas')
+    canvas.height = 500
+    canvas.width = 500
+    const ctx = canvas.getContext('2d')
+    ctx.fillStyle = 'red'
+    ctx.fillRect(20,30,40,50)
+    ctx.beginPath();
+    ctx.arc(100, 200, 60, 30,0, true);
+    ctx.strokeStyle = "green";
+    ctx.lineWidth = 5;
+    ctx.stroke()
+    ctx.fillStyle = 'yellow'
+    ctx.fill()
 });
